c8bb561ac3f33f1805956bfc93216f6030ec16a7
Glenn Downing <downing@cs.utexas.edu>
2014-02-24 17:18:45 -0600

    another commit

--- quizzes/Quiz16.py
+++ quizzes/Quiz16.py
@@ -28,3 +28,32 @@ print(x.v)
 
 x.v += [9]
 print(x.v)
+
+"""
+I made a mistake writing this quiz. Below is the quiz I intended.
+"""
+
+""" ----------------------------------------------------------------------
+ 1. What is the output of the following?
+    (4 pts)
+
+[2, 3, 4]
+[2, 3, 4, 5]
+[6, 7, 8]
+[6, 7, 8, 9]
+"""
+
+class A :
+    v = [2, 3, 4]
+
+x = A()
+print(A.v)
+
+x.v += [5]
+print(A.v)
+
+x.v = [6, 7, 8]
+print(A.v)
+
+x.v += [9]
+print(A.v)
