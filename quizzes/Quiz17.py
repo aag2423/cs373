e4fedcc9ac2720a49719c672e697cca085028949
Glenn Downing <downing@cs.utexas.edu>
2014-02-26 17:02:54 -0600

    another commit

---
+++ quizzes/Quiz17.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+CS373: Quiz #17 (5 pts) <Prateek>
+"""
+
+""" ----------------------------------------------------------------------
+ 1. What is the output of the following?
+    (4 pts)
+
+True
+True
+False
+False
+"""
+
+from copy import copy, deepcopy
+
+class A :
+    def __init__ (self) :
+        self.a = [2, [3], 4]
+
+x = A()
+y = copy(x)
+
+print(x.a    is y.a)
+print(x.a[1] is y.a[1])
+
+z = deepcopy(x)
+
+print(x.a    is z.a)
+print(x.a[1] is z.a[1])
