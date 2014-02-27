059d5e4c3e6fc1fd05e9b2f4a8e528f488993036
Glenn Downing <downing@cs.utexas.edu>
2014-02-27 15:59:47 -0600

    another commit

--- examples/Variables.py
+++ examples/Variables.py
@@ -47,14 +47,33 @@ assert(a is b)
 a = [2, 3, 4]
 b = a
 assert(a is b)
+b = b + [5]
+assert(a == [2, 3, 4])
+assert(b == [2, 3, 4, 5])
+
+a = [2, 3, 4]
+b = a
+assert(a is b)
 b += (5,)
 assert(a == [2, 3, 4, 5])
 assert(a is b)
+
+a = [2, 3, 4]
+b = a
+assert(a is b)
+#b = b + (5,)  # TypeError: can only concatenate list (not "tuple") to list
 
 a = (2, 3, 4)
 b = a
 assert(a is b)
 b += (5,)
+assert(a == (2, 3, 4))
+assert(b == (2, 3, 4, 5))
+
+a = (2, 3, 4)
+b = a
+assert(a is b)
+b = b + (5,)
 assert(a == (2, 3, 4))
 assert(b == (2, 3, 4, 5))
 
@@ -67,5 +86,10 @@ try :
 except TypeError as e :
     assert(len(e.args) == 1)
     assert(e.args      == ('can only concatenate tuple (not "list") to tuple',))
+
+a = (2, 3, 4)
+b = a
+assert(a is b)
+#b = b + [5]   # TypeError: can only concatenate tuple (not "list") to tuple
 
 print("Done.")
