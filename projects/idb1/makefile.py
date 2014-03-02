1847a13c0a1fe0d0769db5e3ea3f52af5ef744b6
Glenn Downing <downing@cs.utexas.edu>
2014-03-02 17:16:03 -0600

    another commit

--- projects/idb1/makefile.py
+++ projects/idb1/makefile.py
@@ -1,33 +1,8 @@
 all:
-	make IDB.zip
 
 clean:
 	rm -f *.pyc
 	rm -f Models.html
-	rm -f IDB.log
-	rm -f IDB.zip
-
-turnin-list:
-	turnin --list acoomans cs373pj3
-
-turnin-submit: IDB.zip
-	turnin --submit acoomans cs373pj3 IDB.zip
-
-turnin-verify:
-	turnin --verify acoomans cs373pj3
 
 Models.html: Models.py
 	epydoc IDB
-
-IDB.log:
-	git log > IDB.log
-
-IDB.zip: makefile apiary.apib   \
-         Models.html Models.py  \
-         IDB.log     Report.pdf \
-         test.py
-	zip -r IDB.zip                 \
-           makefile    apiary.apib \
-           Models.html Models.py   \
-           IDB.log     Report.pdf  \
-           test.py
